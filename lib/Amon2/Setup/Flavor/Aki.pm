8f6fa2ea662c42fd9c5f321f7dd02d12dbcd8147
akihiro <akihiro@akihiro-mba.local>
2014-06-21 21:56:17 +0900

    initial commit

---
+++ lib/Amon2/Setup/Flavor/Aki.pm
@@ -0,0 +1,32 @@
+package Amon2::Setup::Flavor::Aki;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Amon2::Setup::Flavor::Aki -
+
+=head1 SYNOPSIS
+
+  use Amon2::Setup::Flavor::Aki;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::Aki is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
